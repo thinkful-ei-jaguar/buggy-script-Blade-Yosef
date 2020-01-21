8ff680ea08a277134f539246517394940c075f84
Blade Boles <bladerboles@gmail.com>
2020-01-21 10:07:39 -0700

    Solved bug

---
+++ buggy-script.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const path = require('path');
+
+const source = process.argv[2];
+console.log(source);
+const target = process.argv[3];
+console.log(target);
+// read contents of source
+const contentsOfSource = fs.readFileSync(source, 'utf-8');
+
+// get lines of source into an array, remove empty lines
+const linesInSource = contentsOfSource.split('\n').filter(Boolean);
+
+// make the target dir if it doesn't exist
+if (!fs.existsSync(target)) {
+  fs.mkdirSync(target);
+}
+
+// iterare over the lines
+linesInSource.forEach(line => {
+  // get the content of the lines, first word is a filename, rest is content
+  let [ filename, ...contentArr ] = line.split(' ');
+  // replace commas with empty spaces
+  contentArr = contentArr.join(' ');
+  // construct the full path for the file to create
+  const newFilePath = path.join(__dirname, target, filename);
+
+  // write the file and it's contents
+  fs.writeFileSync(
+    newFilePath,
+    contentArr,
+    { flag: 'w+', encoding: 'utf-8' }
+  );
+});
